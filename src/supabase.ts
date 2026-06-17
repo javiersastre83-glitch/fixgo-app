d48b125d129a5af558b4cb1cf6cc799d198dcc54
javiersastre83-glitch <javiersastre83@gmail.com>
2026-06-17 08:45:03 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://kvemmluxgdlhandjpbfn.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2ZW1tbHV4Z2RsaGFuZGpwYmZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NTM3MjcsImV4cCI6MjA5NTQyOTcyN30.3lAmFsL12eCFz159F3PseXGAvydPFxpc5edI5LWIA7E'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    flowType: 'pkce',
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+})
