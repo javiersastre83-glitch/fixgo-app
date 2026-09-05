7a5cbd75b21eaa3b2c18b30d6d416ffc5df88d31
javiersastre83-glitch <javiersastre83@gmail.com>
2026-09-05 17:26:52 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.fixgo.app',
+  appId: 'ar.fixgo.app',
   appName: 'Fixgo',
   webDir: 'dist'
 };
