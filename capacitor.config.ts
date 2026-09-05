b29fbc5c8a9bf054affb70030302821df3530424
javiersastre83-glitch <javiersastre83@gmail.com>
2026-09-05 09:24:25 -0300

    Agregar cascarón Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fixgo.app',
+  appName: 'Fixgo',
+  webDir: 'dist'
+};
+
+export default config;
